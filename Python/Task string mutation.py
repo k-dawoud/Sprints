d57285ab0b3f2c535c627f89825edbb333767775
k-dawoud <81019730+k-dawoud@users.noreply.github.com>
2023-06-03 13:16:40 +0300

    Update Task string mutation.py

--- Python/Task string mutation.py
+++ Python/Task string mutation.py
@@ -1,4 +1,5 @@
 def function(string='abracadabra',position='5',character='k'):
-    string = string[:5] + character + string[6:] # Write "abrac" then 'k' then "dabra" 
-    print(string) # Print the new string
-function() # Call function
+    string = string[:5] + character + string[6:]
+    print(string)
+
+function()
